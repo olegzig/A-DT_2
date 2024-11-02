b0e82284f64b336dc5ccec005f868e955ac92479
olegzig <boris37544@gmail.com>
2024-11-02 16:02:33 +0300

    Базу сделал

--- LR_4/Program.cs
+++ LR_4/Program.cs
@@ -2,160 +2,155 @@
 
 class BTreeNode
 {
-    public int NumKeys { get; set; } // Количество ключей в узле
-    public int[] Keys { get; set; } // Массив ключей
-    public BTreeNode[] Children { get; set; } // Массив указателей на дочерние узлы
-    public bool IsLeaf { get; set; } // True, если узел является листом
+    public int numKeys; // Количество ключей в узле
+    public int[] keys; // Массив ключей
+    public BTreeNode[] children; // Массив указателей на дочерние узлы
+    public bool isLeaf; // True, если узел является листом
 
-    public BTreeNode(int order, bool isLeaf)
+    public BTreeNode(int degree, bool isLeaf)
     {
-        Keys = new int[order - 1];
-        Children = new BTreeNode[order];
-        NumKeys = 0;
-        IsLeaf = isLeaf;
+        this.isLeaf = isLeaf;
+        keys = new int[degree - 1];
+        children = new BTreeNode[degree];
+        numKeys = 0;
     }
 }
 
 class BTree
 {
     private BTreeNode root;
-    private readonly int order;
+    private int degree;
 
-    public BTree(int order)
+    public BTree(int degree)
     {
-        this.order = order;
+        this.degree = degree;
         root = null;
     }
 
-    // Функция для вставки ключа в B-дерево
-    public void Insert(int key)
+    // Разделение полного дочернего узла
+    private void SplitChild(BTreeNode parent, int index)
     {
-        if (root == null)
+        BTreeNode child = parent.children[index];
+        BTreeNode newNode = new BTreeNode(degree, child.isLeaf);
+        newNode.numKeys = degree / 2 - 1;
+
+        // Перемещение ключей и дочерних узлов в новый узел
+        for (int i = 0; i < degree / 2 - 1; i++)
         {
-            root = new BTreeNode(order, true);
-            root.Keys[0] = key;
-            root.NumKeys = 1;
+            newNode.keys[i] = child.keys[i + degree / 2];
         }
-        else
+
+        if (!child.isLeaf)
         {
-            if (root.NumKeys == order - 1)
+            for (int i = 0; i < degree / 2; i++)
             {
-                BTreeNode newRoot = new BTreeNode(order, false);
-                newRoot.Children[0] = root;
-                SplitChild(newRoot, 0);
-                int index = newRoot.Keys[0] < key ? 1 : 0;
-                InsertNonFull(newRoot.Children[index], key);
-                root = newRoot;
-            }
-            else
-            {
-                InsertNonFull(root, key);
+                newNode.children[i] = child.children[i + degree / 2];
             }
         }
+
+        child.numKeys = degree / 2 - 1;
+
+        // Сдвиг дочерних узлов родителя
+        for (int i = parent.numKeys; i > index; i--)
+        {
+            parent.children[i + 1] = parent.children[i];
+        }
+        parent.children[index + 1] = newNode;
+
+        // Сдвиг ключей родителя
+        for (int i = parent.numKeys - 1; i >= index; i--)
+        {
+            parent.keys[i + 1] = parent.keys[i];
+        }
+        parent.keys[index] = child.keys[degree / 2 - 1];
+        parent.numKeys++;
     }
 
-    // Вставка ключа в неполный узел
+    // Вставка в неполный узел
     private void InsertNonFull(BTreeNode node, int key)
     {
-        int i = node.NumKeys - 1;
+        int i = node.numKeys - 1;
 
-        if (node.IsLeaf)
+        if (node.isLeaf)
         {
-            while (i >= 0 && node.Keys[i] > key)
+            // Вставка ключа в отсортированном порядке
+            while (i >= 0 && node.keys[i] > key)
             {
-                node.Keys[i + 1] = node.Keys[i];
+                node.keys[i + 1] = node.keys[i];
                 i--;
             }
-            node.Keys[i + 1] = key;
-            node.NumKeys++;
+            node.keys[i + 1] = key;
+            node.numKeys++;
         }
         else
         {
-            while (i >= 0 && node.Keys[i] > key)
+            // Поиск дочернего узла для вставки ключа
+            while (i >= 0 && node.keys[i] > key)
             {
                 i--;
             }
             i++;
 
-            if (node.Children[i].NumKeys == order - 1)
+            if (node.children[i].numKeys == degree - 1)
             {
+                // Разделение дочернего узла, если он полный
                 SplitChild(node, i);
-                if (node.Keys[i] < key)
+                if (node.keys[i] < key)
                 {
                     i++;
                 }
             }
-            InsertNonFull(node.Children[i], key);
+            InsertNonFull(node.children[i], key);
         }
     }
 
-    // Функция для разделения полного дочернего узла
-    private void SplitChild(BTreeNode parent, int index)
+    // Вставка ключа в B-дерево
+    public void Insert(int key)
     {
-        BTreeNode child = parent.Children[index];
-        BTreeNode newNode = new BTreeNode(order, child.IsLeaf);
-
-        newNode.NumKeys = order / 2 - 1;
-
-        // Перемещение ключей и дочерних узлов в новый узел
-        for (int i = 0; i < order / 2 - 1; i++)
+        if (root == null)
         {
-            newNode.Keys[i] = child.Keys[i + order / 2];
+            root = new BTreeNode(degree, true);
+            root.keys[0] = key;
+            root.numKeys = 1;
         }
-
-        if (!child.IsLeaf)
+        else
         {
-            for (int i = 0; i < order / 2; i++)
+            if (root.numKeys == degree - 1)
             {
-                newNode.Children[i] = child.Children[i + order / 2];
+                BTreeNode newRoot = new BTreeNode(degree, false);
+                newRoot.children[0] = root;
+                SplitChild(newRoot, 0);
+                root = newRoot;
             }
+            InsertNonFull(root, key);
         }
-
-        child.NumKeys = order / 2 - 1;
-
-        // Сдвиг дочерних узлов родителя, чтобы освободить место для нового узла
-        for (int i = parent.NumKeys; i > index; i--)
-        {
-            parent.Children[i + 1] = parent.Children[i];
-        }
-
-        parent.Children[index + 1] = newNode;
-
-        // Сдвиг ключей родителя, чтобы вставить средний ключ из дочернего узла
-        for (int i = parent.NumKeys - 1; i >= index; i--)
-        {
-            parent.Keys[i + 1] = parent.Keys[i];
-        }
-
-        parent.Keys[index] = child.Keys[order / 2 - 1];
-        parent.NumKeys++;
     }
 
-    // Функция для обхода и печати B-дерева в порядке
-    public void Traverse()
-    {
-        Traverse(root);
-    }
-
-    private void Traverse(BTreeNode node)
+    // Обход B-дерева в порядке возрастания
+    public void Traverse(BTreeNode node)
     {
         if (node != null)
         {
-            for (int i = 0; i < node.NumKeys; i++)
+            for (int i = 0; i < node.numKeys; i++)
             {
-                Traverse(node.Children[i]);
-                Console.Write(node.Keys[i] + " ");
+                Traverse(node.children[i]);
+                Console.Write(node.keys[i] + " ");
             }
-            Traverse(node.Children[node.NumKeys]);
+            Traverse(node.children[node.numKeys]);
         }
+    }
+
+    public void Traverse()
+    {
+        Traverse(root);
     }
 }
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        BTree bTree = new BTree(5); // Создание B-дерева порядка 5
+        BTree bTree = new BTree(4); // Степень B-дерева
 
         bTree.Insert(10);
         bTree.Insert(20);
@@ -164,7 +159,7 @@ class Program
         bTree.Insert(12);
         bTree.Insert(30);
 
-        Console.WriteLine("Обход B-дерева в порядке: ");
+        Console.Write("Обход B-дерева в порядке возрастания: ");
         bTree.Traverse();
         Console.WriteLine();
     }
